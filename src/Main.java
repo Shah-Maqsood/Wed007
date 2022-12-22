e4011e88dcbf3b79efca9ffb770fcde0383b2853
Shah-Maqsood <koelnbonn99@gmail.com>
2022-12-22 18:01:06 +0100

    Commit 01, Wed007

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Main branch");
+        System.out.println("Main branch1");
+        System.out.println("Main branch2");
+        System.out.println("Main branch3");
+        System.out.println("Main branch4");
+        System.out.println("Main branch5");
+        System.out.println("Main branch6");
     }
 }
